bc07b09cb38af9f4badb3c1570ffae1184bd2a9f
Alicoder001 <coderkuchkarov@gmail.com>
2024-03-22 11:16:07 +0500

    localstorage

--- src/app/rootProvider.tsx
+++ src/app/rootProvider.tsx
@@ -1,19 +1,18 @@
 "use client";
 import { setMode } from "@/redux/slice/service";
 import { RootState } from "@/redux/store";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 const RootProvider = ({ children }: { children: ReactNode }) => {
   const dispatch = useDispatch();
-  const localMode = localStorage.getItem("dark") as string;
-  const mode = localMode && JSON.parse(localMode);
-  const width = document.getElementById("sidebar");
-  console.log(width);
-
-  if (mode === true) {
-    dispatch(setMode(!localMode));
-  }
+  useEffect(() => {
+    const localMode = localStorage.getItem("dark") as string;
+    const mode = localMode && JSON.parse(localMode);
+    if (mode === true) {
+      dispatch(setMode(!localMode));
+    }
+  }, []);
 
   const { dark } = useSelector((state: RootState) => state.service);
   return <div className={`root-provider ${dark && "dark"}`}>{children}</div>;
